aa1f0bdcba0e65107f302372b00c6dae3132b25d
BONNe <bonne@bonne.id.lv>
2019-01-31 20:25:32 +0200

    Rename PluginConfig to Settings. - Settings is now correct Config for Addon. Improve Warp class a bit: - add some javaDoc. - load only if hooked.

---
+++ src/main/java/world/bentobox/warps/config/Settings.java
@@ -0,0 +1,142 @@
+package world.bentobox.warps.config;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import world.bentobox.bentobox.api.configuration.ConfigComment;
+import world.bentobox.bentobox.api.configuration.ConfigEntry;
+import world.bentobox.bentobox.api.configuration.StoreAt;
+import world.bentobox.bentobox.database.objects.DataObject;
+
+
+@StoreAt(filename="config.yml", path="addons/WelcomeWarps")
+@ConfigComment("WelcomeWarps Configuration [version]")
+@ConfigComment("This config file is dynamic and saved when the server is shutdown.")
+@ConfigComment("You cannot edit it while the server is running because changes will")
+@ConfigComment("be lost! Use in-game settings GUI or edit when server is offline.")
+@ConfigComment("")
+public class Settings implements DataObject
+{
+    @ConfigComment("")
+    @ConfigComment("Warp Restriction - needed levels to be able to create a warp")
+    @ConfigComment("0 or negative values will disable this restriction 10 is default")
+    @ConfigEntry(path = "warplevelrestriction")
+    private int warpLevelRestriction;
+
+    @ConfigComment("")
+    @ConfigComment("Text that player must put on sign to make it a warp sign")
+    @ConfigComment("Not case sensitive!")
+    @ConfigEntry(path = "welcomeLine")
+    private String welcomeLine;
+
+    @ConfigComment("")
+    @ConfigComment("This list stores GameModes in which Level addon should not work.")
+    @ConfigComment("To disable addon it is necessary to write its name in new line that starts with -. Example:")
+    @ConfigComment("disabled-gamemodes:")
+    @ConfigComment(" - BSkyBlock")
+    @ConfigEntry(path = "disabled-gamemodes")
+    private Set<String> disabledGameModes = new HashSet<>();
+
+    @ConfigComment("")
+    private String uniqueId = "config";
+
+// ---------------------------------------------------------------------
+// Section: Constructor
+// ---------------------------------------------------------------------
+
+
+    /**
+     * Loads the various settings from the config.yml file into the plugin
+     */
+    public Settings()
+    {
+        // empty constructor
+    }
+
+
+// ---------------------------------------------------------------------
+// Section: Methods
+// ---------------------------------------------------------------------
+
+
+    /**
+     * @return the warpLevelRestriction
+     */
+    public int getWarpLevelRestriction()
+    {
+        return warpLevelRestriction;
+    }
+
+
+    /**
+     * @return the uniqueId
+     */
+    @Override
+    public String getUniqueId()
+    {
+        return uniqueId;
+    }
+
+
+    /**
+     * @param uniqueId - unique ID the uniqueId to set
+     */
+    @Override
+    public void setUniqueId(String uniqueId)
+    {
+        this.uniqueId = uniqueId;
+    }
+
+
+    /**
+     * This method sets the warpLevelRestriction object value.
+     * @param warpLevelRestriction the warpLevelRestriction object new value.
+     *
+     */
+    public void setWarpLevelRestriction(int warpLevelRestriction)
+    {
+        this.warpLevelRestriction = warpLevelRestriction;
+    }
+
+
+    /**
+     * This method returns the welcomeLine object.
+     * @return the welcomeLine object.
+     */
+    public String getWelcomeLine()
+    {
+        return welcomeLine;
+    }
+
+
+    /**
+     * This method sets the welcomeLine object value.
+     * @param welcomeLine the welcomeLine object new value.
+     *
+     */
+    public void setWelcomeLine(String welcomeLine)
+    {
+        this.welcomeLine = welcomeLine;
+    }
+
+
+    /**
+     * This method returns the disabledGameModes object.
+     * @return the disabledGameModes object.
+     */
+    public Set<String> getDisabledGameModes()
+    {
+        return disabledGameModes;
+    }
+
+
+    /**
+     * This method sets the disabledGameModes object value.
+     * @param disabledGameModes the disabledGameModes object new value.
+     *
+     */
+    public void setDisabledGameModes(Set<String> disabledGameModes)
+    {
+        this.disabledGameModes = disabledGameModes;
+    }
+}
