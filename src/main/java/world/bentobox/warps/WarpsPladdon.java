0003c91cca87d83096f73ae20dc83d2c80adc4b2
tastybento <tastybento@wasteofplastic.com>
2021-08-08 16:40:13 -0700

    Make Warps a Pladdon.

---
+++ src/main/java/world/bentobox/warps/WarpsPladdon.java
@@ -0,0 +1,12 @@
+package world.bentobox.warps;
+
+import world.bentobox.bentobox.api.addons.Addon;
+import world.bentobox.bentobox.api.addons.Pladdon;
+
+public class WarpsPladdon extends Pladdon {
+
+    @Override
+    public Addon getAddon() {
+        return new Warp();
+    }
+}
