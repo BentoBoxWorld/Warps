fe6b020ac8485f5845c087a9e2015c742a296ceb
BONNe <bonne@bonne.id.lv>
2023-04-08 19:10:32 +0300

    Update WarpsPladdon.java

--- src/main/java/world/bentobox/warps/WarpsPladdon.java
+++ src/main/java/world/bentobox/warps/WarpsPladdon.java
@@ -1,13 +1,10 @@
 package world.bentobox.warps;
 
-import org.bukkit.plugin.java.annotation.plugin.ApiVersion;
-import org.bukkit.plugin.java.annotation.plugin.Plugin;
 
 import world.bentobox.bentobox.api.addons.Addon;
 import world.bentobox.bentobox.api.addons.Pladdon;
 
-@Plugin(name="Warps", version="1.0")
-@ApiVersion(ApiVersion.Target.v1_16)
+
 public class WarpsPladdon extends Pladdon {
 
     @Override
